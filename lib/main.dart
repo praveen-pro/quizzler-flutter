306f2bad3aec712f89385e5c216a46c6ad8370a1
praveen <praveenj.pro@gmail.com>
2019-07-22 09:12:25 +0530

    Added scoreKeeper and questions

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,16 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow downstairs but not upstairs',
+    'Approximately one quarter of human bones are in the feet',
+    'A slug\'s blood is green'
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +72,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                nextQuestion();
               },
             ),
           ),
@@ -80,13 +91,22 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                nextQuestion();
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
+  }
+
+  void nextQuestion() {
+    setState(() {
+      if (questionNumber < questions.length - 1) questionNumber++;
+    });
   }
 }
 
